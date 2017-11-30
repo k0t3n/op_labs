c2740528edb6a616ace0d4c61da4e2bc12f7130d
k0t3n <k0t3n.mail@gmail.com>
2017-11-30 14:19:57 +0300

    Add recent labs

--- lab15/lab15/main.c
+++ lab15/lab15/main.c
@@ -4,35 +4,37 @@
 //
 //  Created by Алексей Котенко on 24.11.2017.
 //  Copyright © 2017 Алексей Котенко. All rights reserved.
-// Переписать в массив Y подряд положительные элементы массива X 
+//
+// Вычислить сумму и количество элементов массива X, удовлетворяющих условию 0≤Xi≤1
 
 #include <stdio.h>
 #define HB 5    //размер массива
 
 int main()
 {
-    int a[HB];    // массив
-    int b[HB];    // массив
+    float a[HB];    // массив
     int i;    // индекс массива
-    int g = 1;
-
+    float summ = 0.0;
+    int count = 0;
+    
     printf("\nВведите в одной строке %i чисел, и нажмите ENTER\n",HB);
-
+    
     // Инициализация массива
     for ( i = 0; i < HB; i++ ) {
-        scanf("%i", &a[i]);
+        scanf("%f", &a[i]);
     }
-
+    
     for (i = 0; i < HB; i++ ){
-        if (a[i] > 0){
-            b[g] = a[i];
-            g++;
+        if (0 <= a[i] & a[i] <= 1) {
+            count += 1;
+            summ += a[i];
         }
     }
-
-
-
+    
+    printf("\nКоличество - %i\nСумма - %.2f\n",  count, summ);
+    
 }
+
 
 
 
