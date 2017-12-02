f5ad2134d72499aeb21b5fc5247db750817dfca2
k0t3n <k0t3n.mail@gmail.com>
2017-12-02 21:35:28 +0300

    Add recent labs

--- lab18/lab18/main.c
+++ lab18/lab18/main.c
@@ -9,39 +9,27 @@
 // заполнить нулями
 
 #include <stdio.h>
-#define HB 5    //размер массива
+#define HB 10    //размер массива
 
 int main()
 {
     float a[HB];    // массив
     int i;    // индекс массива
-    float tmp;
-    int selecter;
+    int tmp;
     
     printf("\nВведите в одной строке %i чисел, и нажмите ENTER\n",HB);
     
     // Инициализация массива
-    for ( i = 0; i < HB; i++ ) {
+    for (i = 0; i < HB; i++) {
         scanf("%f", &a[i]);
     }
     
     for (i = 0; i < HB; i++ ){
-        if (abs(a[i]) <= 1) {
-            a[i] = 0;
-        }
+        a[i];
     }
-    while (selecter == 1) {
-        selecter = 0;
-        for (i = 0; i < HB; i++ ){
-            if (abs(a[i]) == 0 & abs(a[i -1] != 0)) {
-                tmp = a[i + 1];
-                a[i + 1] = a[i];
-                a[i] = tmp;
-                selecter = 1;
-                puts("kek");
-            }
-        }
-    }
+    
+    
+    
     
     
     for (i = 1; i <= HB; i++ ){
@@ -50,6 +38,7 @@ int main()
     
     
 }
+
 
 
 
